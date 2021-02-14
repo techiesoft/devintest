e0f5028c92fa354b0c87e64e0957a437acc50147
techiesoft <techiesoftltd@gmail.com>
2021-02-14 19:06:29 +0000

    Jira 001 - Added new method

--- src/main/java/AppClass1.java
+++ src/main/java/AppClass1.java
@@ -3,4 +3,8 @@ public class AppClass1 {
     public void doSomething(){
         System.out.println("Hello Git Hub");
     }
+
+    public void doSomethingElse(){
+        System.out.println("so far so good");
+    }
 }
